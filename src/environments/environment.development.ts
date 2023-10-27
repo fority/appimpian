1151a634128aca8d0dcd80811e984f3f36d516ca
yuen kai <yuenkai@outlook.com>
2023-10-27 12:38:03 +0800

    update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://impianapi.dkcorp.com',
-  // OidcBaseUrl: 'https://localhost:4200',
-
   RedirectUrl: 'https://localhost:4200',
   AuthServerUrl: 'https://authserver.dkproperties.com.my',
 };
